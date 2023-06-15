f07e4a014621bad63ffc84521b2f9e622715a111
terra456 <terra.456@yandex.ru>
2023-06-15 14:59:06 +0500

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import path from 'path';
+import { readdir } from 'node:fs/promises';
+import { getDirAndFile } from '../utils.js';
+
+const { __dirname } = getDirAndFile(import.meta.url);
+const sourceDir = 'files';
+
 const list = async () => {
-    // Write your code here 
+    const sourcePath = path.resolve(__dirname, sourceDir);
+    readdir(sourcePath, {withFileTypes: true})
+        .then((files) => {
+            for (const file of files) {
+                if (file.isFile) {
+                    console.log(path.parse(file.name).name);
+                }
+            }
+        })
+        .catch(() => {
+            console.log('FS operation failed: no such folder');
+        })
 };
 
 await list();
