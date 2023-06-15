2eca71892154a272afe4bfa59e75c843b21855b1
terra456 <terra.456@yandex.ru>
2023-06-15 16:29:33 +0500

    feat: calculate hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { stdout } from 'node:process';
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'fs';
+import { getDirAndFile } from '../utils.js';
+
+const { __dirname } = getDirAndFile(import.meta.url);
+const dirName = 'files';
+const fileName = 'fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const pathToFile = path.resolve(__dirname, dirName, fileName);
+    const hash = createHash('sha256');
+    const input = createReadStream(pathToFile);
+    input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
