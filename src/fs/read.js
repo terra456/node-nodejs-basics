c09d27486a1bece6878a2eadf13b732326067b09
terra456 <terra.456@yandex.ru>
2023-06-16 10:05:59 +0500

    feat: implement function to read file by fs module

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import path from 'path';
+import { readFile } from 'node:fs/promises';
+import { getDirAndFile } from '../utils.js';
+
+const { __dirname } = getDirAndFile(import.meta.url);
+const dirName = 'files';
+const fileName = 'fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = path.resolve(__dirname, dirName, fileName);
+    readFile(pathToFile, { encoding: 'utf8' })
+        .then((data) => console.log(data))
+        .catch((e) => console.log('FS operation failed'));
 };
 
 await read();
