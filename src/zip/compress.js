8d4d6b17bdd55225fdcb693fab32f737795dcafe
terra456 <terra.456@yandex.ru>
2023-06-16 11:37:36 +0500

    feat: compress and decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,31 @@
+import path from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { getDirAndFile } from '../utils.js';
+import { createGzip } from 'zlib';
+
+const { __dirname } = getDirAndFile(import.meta.url);
+const srcDir = 'files';
+const srcName = 'fileToCompress.txt';
+const distDir = srcDir;
+const distName = 'archive.gz';
+
 const compress = async () => {
-    // Write your code here 
+    const srcFile = path.resolve(__dirname, srcDir, srcName);
+    const distFile = path.resolve(__dirname, distDir, distName);
+    const readStream = createReadStream(srcFile);
+    const writeStream = createWriteStream(distFile);
+    const compressStream = createGzip();
+    const onError = () => {
+        console.log('Do not compressed');
+        readStream.destroy();
+        writeStream.end();
+    }
+    readStream
+        .on('error', onError)
+        .pipe(compressStream)
+        .on('error', onError)
+        .pipe(writeStream)
+        .on('error', onError);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,31 @@
+import path from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { getDirAndFile } from '../utils.js';
+import { createGunzip } from 'zlib';
+
+const { __dirname } = getDirAndFile(import.meta.url);
+const srcDir = 'files';
+const srcName = 'archive.gz';
+const distDir = srcDir;
+const distName = 'fileToDecompress.txt';
+
 const decompress = async () => {
-    // Write your code here 
+    const srcFile = path.resolve(__dirname, srcDir, srcName);
+    const distFile = path.resolve(__dirname, distDir, distName);
+    const readStream = createReadStream(srcFile);
+    const writeStream = createWriteStream(distFile);
+    const decompressStream = createGunzip();
+    const onError = () => {
+        console.log('Not decompressed');
+        readStream.destroy();
+        writeStream.end();
+    }
+    readStream
+        .on('error', onError)
+        .pipe(decompressStream)
+        .on('error', onError)
+        .pipe(writeStream)
+        .on('error', onError);
 };
 
 await decompress();
