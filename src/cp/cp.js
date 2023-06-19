c455c9511585f5714354c1a6176011cb9ae55772
terra456 <terra.456@yandex.ru>
2023-06-19 11:05:13 +0500

    feat: child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import path from 'path';
+import { fork } from 'node:child_process';
+import { getDirAndFile } from '../utils.js';
+
+const { __dirname } = getDirAndFile(import.meta.url);
+const dirName = 'files';
+const fileName = 'script.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const pathToFile = path.resolve(__dirname, dirName, fileName);
+    const child = fork(pathToFile, args);
+    child.on('error', (err) => {
+      console.log('Error', err);
+    });
+    child.on('message', (msg) => {
+      console.log('Message from child:', msg);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
