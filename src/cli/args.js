0a2196ed3d67678ad5c9280e67b68ad77a443af7
terra456 <terra.456@yandex.ru>
2023-06-19 18:33:36 +0500

    fix: format of reterned data in cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -3,7 +3,7 @@ import { argv } from 'node:process';
 const parseArgs = () => {
     const res = argv.map((el, i) => {
         if (el.startsWith('--')) {
-            return(`${el} is ${argv[i + 1]}`)
+            return(`${el.replace('--', '')} is ${argv[i + 1]}`)
         }
     }).filter((el) => el);
     console.log(res.join(', '));
